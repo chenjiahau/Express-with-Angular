87176297363991a4b1aed3db5bb66f051cd8ff20
chenjiahau <ivan.chen@dlinkcorp.com>
2022-02-12 09:26:12 +0800

    Move routing setting to another module

---
+++ client/src/app/app-routing.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { GreetingComponent } from './greeting/greeting.component';
+import { LogComponent } from './log/log.component';
+import { LogIntroComponent } from './log/log-intro/log-intro.component';
+import { LogDetailComponent } from './log/log-detail/log-detail.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+const appRoutes: Routes = [
+  // An empty path is the path of every route
+  // So redirect to greeting also redirect empty path, it doesn't work
+  // It need to set pathMath to full
+  { path: '', redirectTo: '/greeting', pathMatch: 'full' },
+  { path: 'greeting', component: GreetingComponent },
+  {
+    path: 'log',
+    component: LogComponent,
+    children: [
+      { path: '', component: LogIntroComponent },
+      { path: ':id', component: LogDetailComponent }
+    ]
+  },
+  { path: 'not-found', component: NotFoundComponent },
+  { path: '**', redirectTo: "/not-found" },
+]
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(appRoutes)
+  ],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule{
+}
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,35 +8,14 @@ import { AppComponent } from './app.component';
 import { GreetingComponent } from './greeting/greeting.component';
 import { AddGreetingComponent } from './add-greeting/add-greeting.component';
 import { SuccessMessageComponent } from './success-message/success-message.component';
-import { LogComponent } from './log/log.component';
-import { NotFoundComponent } from './not-found/not-found.component';
+
+import { AppRoutingModule } from './app-routing.module';
 
 import { FontDirective } from 'src/directives/font.directive';
 import { UnlessDirective } from 'src/directives/unless.directive';
 
 import { GreetingService } from 'src/services/greeting.service';
 import { LogService } from 'src/services/log.service';
-import { LogIntroComponent } from './log/log-intro/log-intro.component';
-import { LogDetailComponent } from './log/log-detail/log-detail.component';
-
-const appRoutes: Routes = [
-  // An empty path is the path of every route
-  // So redirect to greeting also redirect empty path, it doesn't work
-  // It need to set pathMath to full
-  { path: '', redirectTo: '/greeting', pathMatch: 'full' },
-  { path: 'greeting', component: GreetingComponent },
-  {
-    path: 'log',
-    component: LogComponent,
-    children: [
-      { path: '', component: LogIntroComponent },
-      { path: ':id', component: LogDetailComponent }
-    ]
-  },
-  { path: 'not-found', component: NotFoundComponent },
-  { path: '**', redirectTo: "/not-found" },
-]
-
 
 @NgModule({
   declarations: [
@@ -45,16 +24,12 @@ const appRoutes: Routes = [
     AddGreetingComponent,
     SuccessMessageComponent,
     FontDirective,
-    UnlessDirective,
-    LogComponent,
-    LogDetailComponent,
-    NotFoundComponent,
-    LogIntroComponent
+    UnlessDirective
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule
   ],
   providers: [GreetingService, LogService],
   bootstrap: [AppComponent]
