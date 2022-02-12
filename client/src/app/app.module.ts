96411f1c2b5d6fe776363809bcd30c3c8b6d096c
chenjiahau <ivan.chen@dlinkcorp.com>
2022-02-12 17:48:29 +0800

    Don't understand outsourcing routing cause a lot error.
    Mvoe it

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,14 +8,35 @@ import { AppComponent } from './app.component';
 import { GreetingComponent } from './greeting/greeting.component';
 import { AddGreetingComponent } from './add-greeting/add-greeting.component';
 import { SuccessMessageComponent } from './success-message/success-message.component';
-
-import { AppRoutingModule } from './app-routing.module';
+import { LogComponent } from './log/log.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 import { FontDirective } from 'src/directives/font.directive';
 import { UnlessDirective } from 'src/directives/unless.directive';
 
 import { GreetingService } from 'src/services/greeting.service';
 import { LogService } from 'src/services/log.service';
+import { LogIntroComponent } from './log/log-intro/log-intro.component';
+import { LogDetailComponent } from './log/log-detail/log-detail.component';
+
+const appRoutes: Routes = [
+  // An empty path is the path of every route
+  // So redirect to greeting also redirect empty path, it doesn't work
+  // It need to set pathMath to full
+  { path: '', redirectTo: '/greeting', pathMatch: 'full' },
+  { path: 'greeting', component: GreetingComponent },
+  {
+    path: 'log',
+    component: LogComponent,
+    children: [
+      { path: '', component: LogIntroComponent },
+      { path: ':id', component: LogDetailComponent }
+    ]
+  },
+  { path: 'not-found', component: NotFoundComponent },
+  { path: '**', redirectTo: "/not-found" },
+]
+
 
 @NgModule({
   declarations: [
@@ -24,12 +45,16 @@ import { LogService } from 'src/services/log.service';
     AddGreetingComponent,
     SuccessMessageComponent,
     FontDirective,
-    UnlessDirective
+    UnlessDirective,
+    LogComponent,
+    LogDetailComponent,
+    NotFoundComponent,
+    LogIntroComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [GreetingService, LogService],
   bootstrap: [AppComponent]
