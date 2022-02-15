97738c1c1ee556d78a579a0f519a1cfac85bfc06
chenjiahau <chenjiahau@gmail.com>
2022-02-15 19:38:27 +0800

    Simple validor

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Angular';
+export class AppComponent implements OnInit {
+  genderList: string[];
+  ageList: string[];
+  habitList: { title: string; selected: boolean;}[];
+
+  ngOnInit() {
+    this.genderList = ["Male", "Female"];
+    this.ageList = ["Child", "Teenager", "Young", "Old"];
+    this.habitList = [
+      { title: "Hiking", selected: false },
+      { title: "Sports", selected: false },
+      { title: "Sleeping", selected: false },
+    ];
+  }
+
+  onSubmit(questionnarie: NgForm) {
+    console.log(questionnarie);
+  }
 }
