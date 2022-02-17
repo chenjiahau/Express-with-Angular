ed86fa092d453299447ae8cbab854790761e4df8
chenjiahau <chenjiahau@gmail.com>
2022-02-17 19:46:36 +0800

    Initialize reactive form

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,11 +7,25 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  questionnaireForm: FormGroup;
   genderList: string[];
   ageList: string[];
 
   ngOnInit() {
     this.genderList = ["Male", "Female"];
     this.ageList = ["Child", "Teenager", "Young", "Old"];
+
+    this.questionnaireForm = new FormGroup({
+      'email': new FormControl(null),
+      'firstname': new FormControl(null),
+      'lastname': new FormControl(null),
+      'gender': new FormControl(this.genderList[0]),
+      'age': new FormControl(this.ageList[1]),
+      'aboutyou': new FormControl(null)
+    });
+  }
+
+  onSubmit() {
+    console.log(this.questionnaireForm);
   }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +12,7 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     NgbModule,
-    FormsModule
+    ReactiveFormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
