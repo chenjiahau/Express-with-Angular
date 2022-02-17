800350df1ef2e3d1c42c3ef3d684d72b44447417
chenjiahau <chenjiahau@gmail.com>
2022-02-17 19:22:18 +0800

    Initialize form

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -9,31 +9,9 @@ import { NgForm } from '@angular/forms';
 export class AppComponent implements OnInit {
   genderList: string[];
   ageList: string[];
-  data: {
-    email: string;
-    firstname: string;
-    lastname: string;
-    gender: string;
-    age: string;
-    aboutyou: string;
-  }
 
   ngOnInit() {
     this.genderList = ["Male", "Female"];
     this.ageList = ["Child", "Teenager", "Young", "Old"];
-
-    this.data = {
-      email: "my@c.c",
-      firstname: "i",
-      lastname: "i",
-      gender: "Female",
-      age: "Teenager",
-      aboutyou: "aboutyou"
-    }
-  }
-
-  onSubmit(questionnarie: NgForm) {
-    console.log(questionnarie);
-    questionnarie.reset();
   }
 }
